1c8f49a9f373b13fcbf6340caed01618525468af
Frederic Dewinne <fred@continuous.team>
2020-07-03 07:28:22 +0000

    make watchers change persistent

--- bin/increase-watchers.sh
+++ bin/increase-watchers.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sudo sysctl fs.inotify.max_user_watches=582222 && sudo sysctl -p
+sudo sysctl fs.inotify.max_user_watches=582222 && sudo sysctl -w vm.max_map_count=262144 && sudo sysctl -p
+echo "fs.inotify.max_user_watches=582222" | sudo tee -a /etc/sysctl.conf
+echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
