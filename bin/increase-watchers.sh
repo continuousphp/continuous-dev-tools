61dadadee0d50201c9b367f473e9417b13026086
Frédéric Dewinne <fred@continuous.team>
2021-03-09 10:26:35 +0100

    make file executable and change permanent (#2)

--- bin/increase-watchers.sh
+++ bin/increase-watchers.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sudo sysctl fs.inotify.max_user_watches=582222 && sudo sysctl -w vm.max_map_count=262144 && sudo sysctl -p
-echo "fs.inotify.max_user_watches=582222" | sudo tee -a /etc/sysctl.conf
-echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
+echo fs.inotify.max_user_watches=582222 | sudo tee -a /etc/sysctl.conf 
+echo vm.max_map_count=262144 | sudo tee -a /etc/sysctl.conf 
+sudo sysctl -p
+
